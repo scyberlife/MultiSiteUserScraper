b577ac1f373299e0e220c518cd69177514370eee
Sergei Beliaev <s.cyberlife@gmail.com>
2023-04-06 09:20:16 +0600

    1.2

--- config.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Bot, Dispatcher
-from dotenv import load_dotenv
-from os import getenv
-
-load_dotenv()
-bot = Bot(token=getenv('BOT_TOKEN'))
-dp = Dispatcher(bot)
-
-
--- main.py
+++
@@ -1,8 +0,0 @@
-from aiogram.utils import executor
-from commands import all_search
-from config import bot, dp
-
-
-if __name__ == '__main__':
-    dp.register_message_handler(all_search, commands=['search'])
-    executor.start_polling(dp, skip_updates=True)
